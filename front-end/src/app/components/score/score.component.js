298c25341b518efbe8fc5e6e60d55b1fa1bd13bf
Lansana <lansana.diomande@epfedu.fr>
2021-04-23 11:14:29 +0200

    Fin du step 3.1

--- front-end/src/app/components/score/score.component.js
+++ front-end/src/app/components/score/score.component.js
@@ -1,46 +1,46 @@
 // TODO Step 7 import "./score.components.html"
 
-(function() {      // TODO Step 7 remove this closure
+(function () {      // TODO Step 7 remove this closure
 
-    // TODO Step 3.1 create a class
     /* class ScoreComponent constructor */
-    function ScoreComponent() {
-        var params = parseUrl();
-        this.name = params.name;
-        this.size = parseInt(params.size);
-        this.time = parseInt(params.time);
-    }
-
-    /* method ScoreComponent.init */
-    ScoreComponent.prototype.init = init;
-
-    function init() {
-        document.getElementById('name').innerText = this.name;
-        document.getElementById('size').innerText = this.size;
-        document.getElementById('time').innerText = this.time;
-    }
-
-    // TODO Step 6 implement getTemplate() {}
-
-    function parseUrl() {
-        var url = window.location;
-        var query = url.href.split('?')[1] || '';
-        var delimiter = '&';
-        var result = {};
-
-        var parts = query
-            .split(delimiter);
-        // TODO Step 3.3: Use Array.map() & Array.reduce()
-        for (var i in parts) {
-            var item = parts[i];
-            var kv = item.split('=');
-            result[kv[0]] = kv[1];
+    class ScoreComponent {
+        constructor() {
+            var params = this.parseUrl();
+            this.name = params.name;
+            this.size = parseInt(params.size);
+            this.time = parseInt(params.time);
         }
 
-        return result;
+        init() {
+            document.getElementById('name').innerText = this.name;
+            document.getElementById('size').innerText = this.size;
+            document.getElementById('time').innerText = this.time;
+        }
+
+        parseUrl() {
+            var url = window.location;
+            var query = url.href.split('?')[1] || '';
+            var delimiter = '&';
+            var result = {};
+            var parts = query
+                .split(delimiter);
+            // TODO Step 3.3: Use Array.map() & Array.reduce()
+            for (var i in parts) {
+                var item = parts[i];
+                var kv = item.split('=');
+                result[kv[0]] = kv[1];
+            }
+
+            return result;
+        }
+
+        // TODO Step 6 implement getTemplate() {}
+
     }
 
     // put components in global scope, tu be runnable right from the HTML.
     // TODO Step 7 export ScoreComponent
+
+
     window.ScoreComponent = ScoreComponent;
 })();
--- front-end/src/app/components/welcome/welcome.component.js
+++ front-end/src/app/components/welcome/welcome.component.js
@@ -2,40 +2,40 @@
 
 (function() {   // TODO Step 7 remove this closure
 
-    // TODO Step 3.1 create a class
     /* class WelcomeComponent constructor  */
-    function WelcomeComponent() {
-    }
 
-    /* method WelcomeComponent.init */
-    WelcomeComponent.prototype.init = init;
+    class WelcomeComponent{
+        constructor() {
+        }
 
-    function init() {
-        var form = document.querySelector('form.form-signin');
+        init() {
+            var form = document.querySelector('form.form-signin');
 
-        form.addEventListener('submit', function(event) {     // TODO Step 3.2: use arrow function
+            form.addEventListener('submit', (event) =>{     // TODO Step 3.2: use arrow function
 
-            event.preventDefault();
-            if (form.checkValidity() === false) {
-                event.stopPropagation();
-                form.classList.add('was-validated');
-            } else {
-                var name = event.srcElement.querySelector('#nickname').value;
-                var size = parseInt(event.srcElement.querySelector('#size').value);
+                event.preventDefault();
+                if (form.checkValidity() === false) {
+                    event.stopPropagation();
+                    form.classList.add('was-validated');
+                } else {
+                    var name = event.srcElement.querySelector('#nickname').value;
+                    var size = parseInt(event.srcElement.querySelector('#size').value);
 
-                _startGame(name, size);
-            }
-        }, false);
+                    this._startGame(name, size);
+                }
+            }, false);
 
-        return this;
-    }
+            return this;
+        }
 
-    // TODO Step 6 implement getTemplate() {}
+        // TODO Step 6 implement getTemplate() {}
 
-    function _startGame(name, size) {
-        // TODO Step 3.2: use template literals
-        // TODO Step 7: change path to: `game?name=${name}=name&size=${size}`
-        window.location = `../game/game.component.html?name=${name}&size=${size}`;
+         _startGame(name, size) {
+            // TODO Step 3.2: use template literals
+            // TODO Step 7: change path to: `game?name=${name}=name&size=${size}`
+            window.location = `../game/game.component.html?name=${name}&size=${size}`;
+        }
+
     }
 
     // put components in global scope, tu be runnable right from the HTML.
